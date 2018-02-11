da9ffc52d9e3ad3e1e14f87ec3e21625f7df0d38
bitcoinz-pod <bitcoinz-pod@crypto-moon.org>
2018-02-11 13:53:35 -0800

    build fix

--- zcutil/build-win.sh
+++ zcutil/build-win.sh
@@ -2,7 +2,8 @@
 
 cd "$(dirname "$(readlink -f "$0")")/.."    #'"%#@!
 
-patch -p 1 < ./zcutil/extra/snark-multiarch.diff
+#Already patched
+#patch -p 1 < ./zcutil/extra/snark-multiarch.diff
 
 HOST=x86_64-w64-mingw32
 CXX=x86_64-w64-mingw32-g++-posix
