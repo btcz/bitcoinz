5e2d1f1fe531f9393b5008efbb36ce6ff6d8117e
renuzit <renuzit@btcz.life>
2018-06-05 00:08:21 -0500

    Bumped the protocol version to 770004

--- src/sendalert.cpp
+++ src/sendalert.cpp
@@ -78,7 +78,7 @@ void ThreadSendAlert()
     // These versions are protocol versions
     // 170002 : 1.0.0
     alert.nMinVer       = 170002;
-    alert.nMaxVer       = 770003;
+    alert.nMaxVer       = 770004;
 
     //
     // main.cpp:
