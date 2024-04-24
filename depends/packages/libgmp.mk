fa08755f788ee9834fbb1ee31e4e8fc53faa9b56
EzzyG <59780480+ezzygarmyz@users.noreply.github.com>
2024-04-25 01:53:39 +0200

    Update libgmp.mk

--- depends/packages/libgmp.mk
+++ depends/packages/libgmp.mk
@@ -1,11 +1,10 @@
 package=libgmp
-$(package)_download_path=https://github.com/z-classic/$(package)/archive/
-$(package)_file_name=$(package)-$($(package)_git_commit).tar.gz
-$(package)_download_file=$($(package)_git_commit).tar.gz
-$(package)_sha256_hash=59b2c2b5d58fdf5943bfde1fa709e9eb53e7e072c9699d28dc1c2cbb3c8cc32c
-$(package)_git_commit=aece03c7b6967f91f3efdac8c673f55adff53ab1
+$(package)_version=6-1-2
+$(package)_download_path=https://github.com/ezzygarmyz/libgmp/releases/download/6.1.2/
+$(package)_file_name=libgmp-$($(package)_version).tar.bz2
+$(package)_sha256_hash=77613c44076fd04b3b14a94e77da8bf8153443cca0f0caa517bc6c46479ea77e
 $(package)_dependencies=
-$(package)_config_opts=--enable-cxx --disable-shared
+$(package)_config_opts=--enable-cxx --disable-shared --with-pic
 
 define $(package)_config_cmds
   $($(package)_autoconf) --host=$(host) --build=$(build)
