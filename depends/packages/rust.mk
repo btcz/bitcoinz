062f385ab93be57dbc850ba24b4152333b8a7c16
EzzyG <59780480+ezzygarmyz@users.noreply.github.com>
2024-04-24 15:15:44 +0200

    Update rust.mk

--- depends/packages/rust.mk
+++ depends/packages/rust.mk
@@ -1,25 +1,33 @@
 package=rust
-$(package)_version=1.32.0
+$(package)_version=1.69.0
 $(package)_download_path=https://static.rust-lang.org/dist
+
 $(package)_file_name_linux=rust-$($(package)_version)-x86_64-unknown-linux-gnu.tar.gz
-$(package)_sha256_hash_linux=e024698320d76b74daf0e6e71be3681a1e7923122e3ebd03673fcac3ecc23810
+$(package)_sha256_hash_linux=2ca4a306047c0b8b4029c382910fcbc895badc29680e0332c9df990fd1c70d4f
 $(package)_file_name_darwin=rust-$($(package)_version)-x86_64-apple-darwin.tar.gz
-$(package)_sha256_hash_darwin=f0dfba507192f9b5c330b5984ba71d57d434475f3d62bd44a39201e36fa76304
+$(package)_sha256_hash_darwin=9818dab2c3726d63dfbfde12c9273e62e484ef6d6f6e05a6431a3e089c335454
+$(package)_file_name_mingw32=rust-$($(package)_version)-x86_64-pc-windows-gnu.tar.gz
+$(package)_sha256_hash_mingw32=9a2a887defb9d0e0aa48fd54237b583a693a2565420b4d90efd0740c0fe69b0d
 
-# Mapping from GCC canonical hosts to Rust targets
-# If a mapping is not present, we assume they are identical
-$(package)_rust_target_x86_64-w64-mingw32=x86_64-pc-windows-gnu
+define $(package)_set_vars
+$(package)_stage_opts=--disable-ldconfig
+$(package)_stage_build_opts=--without=rust-docs-json-preview,rust-docs
+endef
 
-# Mapping from Rust targets to SHA-256 hashes
-$(package)_rust_std_sha256_hash_aarch64-unknown-linux-gnu=346efe3aef2aff7b71a611bf7661bcec5f9bc4025a599c2866ec5fd330247cb9
-$(package)_rust_std_sha256_hash_x86_64-pc-windows-gnu=cad5f1454d591c13eeb3657f1c9dbfeb30e648f59680bd0765b94c63e7afc49e
+ifeq ($(build_os),darwin)
+$(package)_file_name=$($(package)_file_name_darwin)
+$(package)_sha256_hash=$($(package)_sha256_hash_darwin)
+else ifeq ($(host_os),mingw32)
+$(package)_file_name=$($(package)_file_name_mingw32)
+$(package)_sha256_hash=$($(package)_sha256_hash_mingw32)
+else
+$(package)_file_name=$($(package)_file_name_linux)
+$(package)_sha256_hash=$($(package)_sha256_hash_linux)
+endif
 
-ifneq ($(canonical_host),$(build))
-$(package)_rust_target=$(if $($(package)_rust_target_$(canonical_host)),$($(package)_rust_target_$(canonical_host)),$(canonical_host))
-$(package)_exact_file_name=rust-std-$($(package)_version)-$($(package)_rust_target).tar.gz
-$(package)_exact_sha256_hash=$($(package)_rust_std_sha256_hash_$($(package)_rust_target))
+ifeq ($(host_os),mingw32)
 $(package)_build_subdir=buildos
-$(package)_extra_sources=$($(package)_file_name_$(build_os))
+$(package)_extra_sources = $($(package)_file_name_$(build_os))
 
 define $(package)_fetch_cmds
 $(call fetch_file,$(package),$($(package)_download_path),$($(package)_download_file),$($(package)_file_name),$($(package)_sha256_hash)) && \
@@ -31,19 +39,19 @@ define $(package)_extract_cmds
   echo "$($(package)_sha256_hash)  $($(package)_source)" > $($(package)_extract_dir)/.$($(package)_file_name).hash && \
   echo "$($(package)_sha256_hash_$(build_os))  $($(package)_source_dir)/$($(package)_file_name_$(build_os))" >> $($(package)_extract_dir)/.$($(package)_file_name).hash && \
   $(build_SHA256SUM) -c $($(package)_extract_dir)/.$($(package)_file_name).hash && \
-  mkdir $(canonical_host) && \
-  tar --strip-components=1 -xf $($(package)_source) -C $(canonical_host) && \
+  mkdir mingw32 && \
+  tar --strip-components=1 -xf $($(package)_source) -C mingw32 && \
   mkdir buildos && \
   tar --strip-components=1 -xf $($(package)_source_dir)/$($(package)_file_name_$(build_os)) -C buildos
 endef
 
 define $(package)_stage_cmds
-  ./install.sh --destdir=$($(package)_staging_dir) --prefix=$(host_prefix)/native --disable-ldconfig && \
-  ../$(canonical_host)/install.sh --destdir=$($(package)_staging_dir) --prefix=$(host_prefix)/native --disable-ldconfig
+  ./install.sh --destdir=$($(package)_staging_dir) --prefix=$(host_prefix)/native $($(package)_stage_opts) $($(package)_stage_build_opts) && \
+  cp -r ../mingw32/rust-std-x86_64-pc-windows-gnu/lib/rustlib/x86_64-pc-windows-gnu $($(package)_staging_dir)$(host_prefix)/native/lib/rustlib
 endef
 else
 
 define $(package)_stage_cmds
-  ./install.sh --destdir=$($(package)_staging_dir) --prefix=$(host_prefix)/native --disable-ldconfig
+  ./install.sh --destdir=$($(package)_staging_dir) --prefix=$(host_prefix)/native $($(package)_stage_opts) $($(package)_stage_build_opts)
 endef
 endif
