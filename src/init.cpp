61a0a553679a0bdba17940e11bc03d7616e033e1
MarkLTZ <team@litecoinz.info>
2024-07-28 23:28:47 +0000

    Initialize experimental mode in a separate function

--- src/init.cpp
+++ src/init.cpp
@@ -691,6 +691,7 @@ void ThreadImport(std::vector<boost::filesystem::path> vImportFiles)
 bool InitExperimentalMode()
 {
     fExperimentalMode = GetBoolArg("-experimentalfeatures", false);
+
     // Fail if user has set experimental options without the global flag
     if (!fExperimentalMode) {
         if (mapArgs.count("-developerencryptwallet")) {
@@ -699,6 +700,8 @@ bool InitExperimentalMode()
             return InitError(_("Setting the size of shielded pools to zero requires -experimentalfeatures."));
         } else if (mapArgs.count("-paymentdisclosure")) {
             return InitError(_("Payment disclosure requires -experimentalfeatures."));
+        } else if (mapArgs.count("-zmergetoaddress")) {
+            return InitError(_("RPC method z_mergetoaddress requires -experimentalfeatures."));
         } else if (mapArgs.count("-insightexplorer")) {
             return InitError(_("Insight explorer requires -experimentalfeatures."));
         }
@@ -932,21 +935,8 @@ bool AppInit2(boost::thread_group& threadGroup, CScheduler& scheduler)
     const CChainParams& chainparams = Params();
 
     // Set this early so that experimental features are correctly enabled/disabled
-    fExperimentalMode = GetBoolArg("-experimentalfeatures", false);
-
-    // Fail early if user has set experimental options without the global flag
-    if (!fExperimentalMode) {
-        if (mapArgs.count("-developerencryptwallet")) {
-            return InitError(_("Wallet encryption requires -experimentalfeatures."));
-        } else if (mapArgs.count("-developersetpoolsizezero")) {
-            return InitError(_("Setting the size of shielded pools to zero requires -experimentalfeatures."));
-        } else if (mapArgs.count("-paymentdisclosure")) {
-            return InitError(_("Payment disclosure requires -experimentalfeatures."));
-        } else if (mapArgs.count("-zmergetoaddress")) {
-            return InitError(_("RPC method z_mergetoaddress requires -experimentalfeatures."));
-        } else if (mapArgs.count("-insightexplorer")) {
-            return InitError(_("Insight explorer requires -experimentalfeatures."));
-        }
+    if (!InitExperimentalMode()) {
+        return false;
     }
 
     // Make sure enough file descriptors are available
