70a4ab3da564b848dcad3e7a289d174647337f20
MarkLTZ <team@litecoinz.info>
2024-08-27 08:10:20 +0000

    Revert "metrics: Add a progress bar when in Initial Block Download mode"
    This reverts commit ac98d743e5bce7a27f6660a8e5a9cc87c145f345.

--- src/metrics.cpp
+++ src/metrics.cpp
@@ -15,7 +15,6 @@
 #include "utilmoneystr.h"
 #include "utilstrencodings.h"
 
-#include <boost/range/irange.hpp>
 #include <boost/thread.hpp>
 #include <boost/thread/synchronized_value.hpp>
 #include <string>
@@ -268,7 +267,7 @@ std::string DisplaySize(size_t value)
     return strprintf(_("%.2f TiB"), value / coef);
 }
 
-int printStats(bool isScreen, bool mining)
+int printStats(bool mining)
 {
     // Number of lines that are always displayed
     int lines = 5;
@@ -293,38 +292,13 @@ int printStats(bool isScreen, bool mining)
             int downloadPercent = nSizeReindexed * 100 / nFullSizeToReindex;
             std::cout << "      " << _("Reindexing blocks") << " | " << DisplaySize(nSizeReindexed) << " / " << DisplaySize(nFullSizeToReindex) << " (" << downloadPercent << "%, " << height << " " << _("blocks") << ")" << std::endl;
         } else {
-            int nHeaders = currentHeadersHeight;
-            if (nHeaders < 0)
-                nHeaders = 0;
             int netheight = currentHeadersHeight == -1 || currentHeadersTime == 0 ?
-                0 : EstimateNetHeight(params, currentHeadersHeight, currentHeadersTime);
-            if (netheight < nHeaders)
-                netheight = nHeaders;
-            if (netheight <= 0)
-                netheight = 1;
-            int downloadPercent = height * 100 / netheight;
-            std::cout << "     " << _("Downloading blocks") << " | " << height << " (" << nHeaders << " " << _("headers") << ") / ~" << netheight << " (" << downloadPercent << "%)" << std::endl;
-
-            if (isScreen) {
-                // Draw 50-character progress bar, which will fit into a 79-character line.
-                int blockChars = downloadPercent / 2;
-                int headerChars = (nHeaders * 50) / netheight;
-                // Start with background colour reversed for "full" bar.
-                std::cout << "                        | [␛[7m";
-                for (auto i : boost::irange(0, 50)) {
-                    if (i == headerChars) {
-                        // Switch to normal background colour for "empty" bar.
-                        std::cout << "␛[0m";
-                    } else if (i == blockChars) {
-                        // Switch to distinct colour for "headers" bar.
-                        std::cout << "␛[0;43m";
-                    }
-                    std::cout << " ";
-                }
-                // Ensure that colour is reset after the progress bar is printed.
-                std::cout << "␛[0m]" << std::endl;
-                lines++;
+            0 : EstimateNetHeight(params, currentHeadersHeight, currentHeadersTime);
+            int downloadPercent = 0;
+            if (netheight > 0) {
+                downloadPercent = height * 100 / netheight;
             }
+            std::cout << "     " << _("Downloading blocks") << " | " << height << " / ~" << netheight << " (" << downloadPercent << "%)" << std::endl;
         }
     } else {
         std::cout << "           " << _("Block height") << " | " << height << std::endl;
@@ -379,7 +353,7 @@ int printMiningStatus(bool mining)
             }
         }
         lines++;
-    } else if (Params().NetworkIDString() != "main") {
+    } else {
         std::cout << _("You are currently not mining.") << std::endl;
         std::cout << _("To enable mining, add 'gen=1' to your bitcoinz.conf and restart.") << std::endl;
         lines += 2;
@@ -605,7 +579,7 @@ void ThreadShowMetricsScreen()
 #endif
 
         if (loaded) {
-            lines += printStats(isScreen, mining);
+            lines += printStats(mining);
             lines += printMiningStatus(mining);
         }
         lines += printMetrics(cols, mining);
