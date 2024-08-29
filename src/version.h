a69bf1f09024e1570e2b38560f913e60bdba3077
MarkLTZ <team@litecoinz.info>
2024-08-29 08:56:58 +0000

    Remove unused constants

--- src/version.h
+++ src/version.h
@@ -14,9 +14,6 @@ static const int PROTOCOL_VERSION = 770009;
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
 
-//! In this version, 'getheaders' was introduced.
-static const int GETHEADERS_VERSION = 31800;
-
 //! disconnect from peers older than this proto version
 static const int MIN_PEER_PROTO_VERSION = 770007;
 
@@ -26,9 +23,6 @@ static const int CADDR_TIME_VERSION = 31402;
 
 //! BIP 0031, pong message, is enabled for all versions AFTER this one
 static const int BIP0031_VERSION = 60000;
-
-//! "mempool" command, enhanced "getdata" behavior starts with this version
-static const int MEMPOOL_GD_VERSION = 60002;
 
 //! "filter*" commands are disabled without NODE_BLOOM after and including this version
 static const int NO_BLOOM_VERSION = 170004;
