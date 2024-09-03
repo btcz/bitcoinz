f23c5e5a73347f34db158f9e6e742a8081c56d09
MarkLTZ <team@litecoinz.info>
2024-09-03 10:40:37 +0000

    Add blockhash parameter to getrawtransaction

--- src/main.h
+++ src/main.h
@@ -291,7 +291,7 @@ bool IsInitialBlockDownload(const CChainParams& chainParams);
  */
 std::string GetWarnings(const std::string& strFor);
 /** Retrieve a transaction (from memory pool, or from disk, if possible) */
-bool GetTransaction(const uint256 &hash, CTransaction &tx, const Consensus::Params& params, uint256 &hashBlock, bool fAllowSlow = false);
+bool GetTransaction(const uint256& hash, CTransaction& tx, const Consensus::Params& params, uint256& hashBlock, bool fAllowSlow = false, const CBlockIndex* blockIndex = nullptr);
 /** Find the best known block, and make it the tip of the block chain */
 bool ActivateBestChain(CValidationState& state, const CChainParams& chainparams, const CBlock* pblock = NULL);
 CAmount GetBlockSubsidy(int nHeight, const Consensus::Params& consensusParams);
